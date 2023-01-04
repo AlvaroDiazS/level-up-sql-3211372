988076ed580c22f171d642bbae42e2a22b32451a
AlvaroDiazS <64406231+AlvaroDiazS@users.noreply.github.com>
2023-01-04 19:33:33 +0000

    Library exercise made

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,20 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT COUNT(*)
+FROM Books 
+WHERE Title = 'Dracula';
+
+SELECT COUNT(*)
+FROM Loans JOIN Books on Loans.BookID = Books.BookID
+WHERE Books.Title = 'Dracula' AND Loans.ReturnedDate IS NULL;
+
+SELECT 
+  (SELECT COUNT(*)
+    FROM Books 
+    WHERE Title = 'Dracula') -
+  (SELECT COUNT(*)
+    FROM Loans JOIN Books on Loans.BookID = Books.BookID
+    WHERE Books.Title = 'Dracula' 
+    AND Loans.ReturnedDate IS NULL)
+AS TotalBooks;
--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,15 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+SELECT Published, COUNT(DISTINCT Title) as NumberBooks, 
+GROUP_CONCAT(DISTINCT(Title)) -- concat all the titles
+FROM Books 
+GROUP BY Published
+ORDER BY NumberBooks DESC;
+
+SELECT COUNT(Loans.LoanID) as LoanCount, Books.Title
+FROM Loans JOIN Books ON Loans.BookID = Books.BookID
+GROUP BY Books.Title
+ORDER by LoanCount DESC
+LIMIT 5;
